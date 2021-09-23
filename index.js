184bff455dd23638327008f4fb97d71b7a9df29d
enzo <enzoserafin@gmail.com>
2021-09-22 22:13:06 -0300

    Arguments

--- index.js
+++ index.js
@@ -1,39 +1,73 @@
-const { gql, ApolloServer } = require("apollo-server")
+const { gql, ApolloServer } = require("apollo-server");
 
-/*
-Scalar Types
- - Int
- - Float
- - String
- - Boolean
- - ID
-*/
+// Schema
+// Schema Definition Language ou Linguagem de definição de esquema
+// SDL
+
+const produtos = [
+    {
+        id: 1,
+        nome: 'Notebook',
+        valor: 122.45
+    },
+    {
+        id: 2,
+        nome: 'Mouse',
+        valor: 10.00
+    }
+]
+
+const usuarios = [
+    {
+        id: 1,
+        nome: 'Enzo',
+        salario: 1234.56,
+        ativo: true,
+        idade: 37
+    },
+    {
+        id: 2,
+        nome: 'Heitor',
+        salario: 1234.56,
+        ativo: true,
+        idade: 1
+    }
+]
 
 const typeDefs = gql`
-    type Query {
+    type Produto {
+        id: ID
+        nome: String
+        valor: Float
+    }
+    
+    type Usuario {
         idade: Int
         salario: Float
         nome: String
         ativo: Boolean
         id: ID
     }
+
+    type Query {
+        usuarios: [Usuario],
+        produtos: [Produto]
+        usuario(id: Int, nome: String): Usuario
+    }
 `
+
 const resolvers = {
     Query: {
-        idade() {
-            return 18;
+        usuarios() {
+            return usuarios
         },
-        salario() {
-            return 1234, 56
+        usuario(obg, args) {
+            const { id, nome } = args
+            if (id) return usuarios.find(u => u.id === args.id)
+            return usuarios.find(u => u.nome === args.nome)
         },
-        nome() {
-            return 'GraphQL'
-        },
-        ativo() {
-            return true
-        },
-        id() {
-            return 1234564987
+        produtos() {
+            return produtos
         }
     }
 }
